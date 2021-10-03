6e89aed24a50d999f69b830b77df49d827fd11a7
Rafaela <rafaela.azevedosouza@gmail.com>
2021-10-03 21:00:48 +0100

    Changed to workshop with petshop

--- truffle-config.js
+++ truffle-config.js
@@ -2,10 +2,13 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
-    develop: {
+    development: {
       host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+    },
+    develop: {
       port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+    },
+  },
 };
